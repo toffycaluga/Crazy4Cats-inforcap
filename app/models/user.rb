e9e080d3a4b26f81009b539571f91fe506a3e08c
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-23 11:44:16 -0300

    FEAT:Relaciones de comment con post y user establecidas

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, as: :commentable,dependent: :destroy
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :bigint           not null, primary key
+#  content          :text
+#  commentable_type :string           not null
+#  commentable_id   :bigint           not null
+#  user_id          :bigint           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
